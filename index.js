a8d10dcfd4d55ee60f6d29b252f94a265c41ac3e
hamimDev <mdhamimhowladerasif@gmail.com>
2024-03-15 15:55:40 +0600

    get coffee in mongobd

--- index.js
+++ index.js
@@ -27,8 +27,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const coffeeCollection = client.db("coffeeDB").collection("coffee");
 
-    
+    app.get("/coffee", async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/coffee", async (req, res) => {
+      const newCoffee = req.body;
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+      console.log(newCoffee);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
