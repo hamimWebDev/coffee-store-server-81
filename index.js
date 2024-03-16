721e50d61e9e9c4aac630a4d5c1067ff05a91bde
hamimDev <mdhamimhowladerasif@gmail.com>
2024-03-16 11:59:39 +0600

    add delete coffee

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -40,6 +40,13 @@ async function run() {
       const result = await coffeeCollection.insertOne(newCoffee);
       res.send(result);
       console.log(newCoffee);
+    });
+
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
